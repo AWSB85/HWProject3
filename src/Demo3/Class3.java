7a7037ab8a473494cbae13178f9503d01fee8cac
awsbasiya <awsbasiya@localhost>
2023-04-06 17:08:17 -0400

    Class3 commit

---
+++ src/Demo3/Class3.java
@@ -0,0 +1,16 @@
+package Demo3;
+
+public class Class3 {
+	
+	
+public static void main (String [] args) {
+		
+		System.out.println("Class3........");
+		
+		System.out.println("Git Class3.....");
+		
+		
+	}
+
+
+}
